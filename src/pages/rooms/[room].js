793924f1fed0f1b25dbbd5d356776ecd00117efd
seommin <seomm1n@naver.com>
2021-07-23 15:39:55 +0900

    Modify utils

--- src/pages/rooms/[room].js
+++ src/pages/rooms/[room].js
@@ -1,33 +1,11 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
+import ROOM from "@src/utils/room.json";
 
 const Room = () => {
   const router = useRouter();
   const { room } = router.query;
-  let name, min, max, explain;
-
-  if (room == "1") {
-    name = "금원산";
-    min = 8;
-    max = 18;
-    explain =
-      "두 가족 이상이나 지인들과 함께 오붓이 지내길 원하는 분들에게 안성맞춤인 방 2 거실 1의 공간입니다";
-  } else if (room == "2") {
-    name = "거망산";
-    min = 4;
-    max = 6;
-    explain = "계곡 쪽에 위치하며 1층 원룸식 방입니다";
-  } else if (room == "3") {
-    name = "월봉산";
-    min = 4;
-    max = 6;
-    explain = "계곡이 보이며 2층 원룸식 방입니다";
-  } else if (room == "4") {
-    name = "기백산";
-    min = 4;
-    max = 6;
-    explain = "앞마당이 훤히 내다보이는 2층 원룸식 방입니다";
-  }
+  const roomInfo = ROOM[room];
 
   return (
     <>
@@ -134,11 +112,13 @@ const Room = () => {
           </button>
         </div>
       </div>
-      <p className="text-center fs-3 font-square">{name}</p>
+      <p className="text-center fs-3 font-square">{roomInfo.name}</p>
       <p className="text-center font-square fs-5 text-secondary mb-1">
-        기준 {min}명 / 최대 {max}명
+        기준 {roomInfo.min}명 / 최대 {roomInfo.max}명
       </p>
-      <p className="text-center font-square fs-6 text-black-50">{explain}</p>
+      <p className="text-center font-square fs-6 text-black-50">
+        {roomInfo.explain}
+      </p>
     </>
   );
 };
